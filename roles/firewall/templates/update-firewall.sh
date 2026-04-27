6e5ffc540fd64bb84b64209058206046f97407cb
Derick Rethans <github@derickrethans.nl>
2026-04-27 10:54:43 +0100

    Tidy-up firewall script generation so that we can (if we chose to) deploy bunny easily

--- roles/firewall/templates/update-firewall.sh
+++ roles/firewall/templates/update-firewall.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+{# setting variables to simplify conditions #}
+{% set needs_access_from_cdn = (inventory_hostname in groups['service']) %}
+{% set needs_trusted_ip_list = (inventory_hostname in groups['service'] and inventory_hostname != 'service0') %}
 
 # Firewall for {{ inventory_hostname }}
 
-{% if inventory_hostname in groups['service'] %}
+{% if needs_access_from_cdn %}
 function safe_download() {
     URL=$1
     DEST=$2
@@ -15,7 +18,9 @@ function safe_download() {
         mv $TEMP_NAME $DEST
     fi
 }
+{% endif %}
 
+{% if needs_trusted_ip_list %}
 TEMP_REMOTE_PROXY_LIST_FILENAME=/tmp/temporary-remote-proxy-list`xxd -l16 -ps /dev/urandom`
 echo "# This list contains IP addresses for the CDN end points" > $TEMP_REMOTE_PROXY_LIST_FILENAME
 {% endif %}
@@ -30,52 +35,66 @@ ufw allow from {{ hostvars[jumphost].ansible_facts['default_ipv4'].address }} to
 # Block port 80 access, as we're TLS only
 ufw deny WWW
 
-{% if inventory_hostname in groups['service'] %}
+{% if needs_access_from_cdn %}
 # Allow access to port 443 from Myra hosts
-echo "# Myra Nodes:" >> $TEMP_REMOTE_PROXY_LIST_FILENAME
-{% for host in myra_hosts %}
+{%   if needs_trusted_ip_list %}
+{%   endif %}
+{%   for host in myra_hosts %}
 ufw allow from {{ host }} to any app "WWW Secure"
+{%   endfor %}
+{%   if needs_trusted_ip_list %}
+echo "# Myra Nodes:" >> $TEMP_REMOTE_PROXY_LIST_FILENAME
+{%     for host in myra_hosts %}
 echo "{{ host }}" >> $TEMP_REMOTE_PROXY_LIST_FILENAME
-{% endfor %}
+{%     endfor %}
+{%   endif %}
 
 # Allow access to port 443 from Bunny hosts
+{%   if needs_trusted_ip_list %}
 echo "# Bunny Nodes (IPv4):" >> $TEMP_REMOTE_PROXY_LIST_FILENAME
+{%   endif %}
 safe_download https://bunnycdn.com/api/system/edgeserverlist /local/systems/bunny-edgeserverlist.json
 for ip in `cat /local/systems/bunny-edgeserverlist.json | jq -a '.[]' | sed 's/"//g'`; do
     ufw allow from $ip to any app "WWW Secure"
+{%   if needs_trusted_ip_list %}
     echo $ip >> $TEMP_REMOTE_PROXY_LIST_FILENAME
+{%   endif %}
 done
 
+{%   if needs_trusted_ip_list %}
 echo "# Bunny Nodes (IPv6):" >> $TEMP_REMOTE_PROXY_LIST_FILENAME
+{%   endif %}
 safe_download https://bunnycdn.com/api/system/edgeserverlist/IPv6 /local/systems/bunny-edgeserverlist-ipv6.json
 for ip in `cat /local/systems/bunny-edgeserverlist-ipv6.json | jq -a '.[]' | sed 's/"//g'`; do
     ufw allow from $ip to any app "WWW Secure"
+{%   if needs_trusted_ip_list %}
     echo $ip >> $TEMP_REMOTE_PROXY_LIST_FILENAME
+{%   endif %}
 done
 
 # Allow access to port 443 from test hosts
-{% for host in test_hosts %}
+{%   for host in test_hosts %}
 ufw allow from {{ host }} to any app "WWW Secure"
-{% endfor %}
+{%   endfor %}
 {% endif %}
 
 {% if inventory_hostname in groups['static'] %}
 # Allow access on 'static' to port 443 from PECL host(s)
-{% for host in pecl_hosts %}
+{%   for host in pecl_hosts %}
 ufw allow from {{ host }} to any app "WWW Secure"
-{% endfor %}
+{%   endfor %}
 {% endif %}
 
 {% if inventory_hostname in groups['dynamic'] %}
 # Allow access TCP port 53 from EasyDNS transfer machine
-{% for host in easy_dns_hosts %}
+{%   for host in easy_dns_hosts %}
 ufw allow proto tcp from {{ host }} to any app DNS
-{% endfor %}
+{%   endfor %}
 
 # Allow access TCP port 443 from servers that need to post to main
-{% for host in access_to_main_hosts %}
+{%   for host in access_to_main_hosts %}
 ufw allow from {{ host }} to any app "WWW Secure"
-{% endfor %}
+{%   endfor %}
 {% endif %}
 
 {% if inventory_hostname in groups['dynamic'] %}
@@ -88,14 +107,14 @@ done
 
 {% if inventory_hostname in groups['rsync'] %}
 # Allow access to rsync from all services
-{% for service_host in groups['service'] %}
+{%   for service_host in groups['service'] %}
 ufw allow proto tcp from {{ hostvars[service_host].ansible_facts[hostvars[service_host].private_interface | default("eth1")].ipv4.address }} to any port 873
-{% endfor %}
+{%   endfor %}
 
 # Allow rsync from approved hosts
-{% for host in rsync_hosts %}
+{%   for host in rsync_hosts %}
 ufw allow proto tcp from {{ host }} to any port 873
-{% endfor %}
+{%   endfor %}
 {% endif %}
 
 # Allow health check to port 8991
@@ -108,9 +127,9 @@ ufw deny "WWW Secure"
 ufw default deny
 ufw enable
 
-{% if inventory_hostname in groups['service'] %}
+{% if needs_trusted_ip_list %}
 # Copy new RemoteIP list to Apache Configuration Directory
 mv $TEMP_REMOTE_PROXY_LIST_FILENAME "{{ remote_ip_trusted_proxy_list }}"
-
+# And reload Apache
 systemctl reload apache2
 {% endif %}
